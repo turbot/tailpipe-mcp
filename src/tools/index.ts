0231f1f640d3cb64c3b2594445918b6e68474807
Nathan Wallace <nathan@turbot.com>
2025-03-28 14:23:12 -0400

    refactor: Remove inspect and list_tables tools
    Removed deprecated tools: - inspect_database - inspect_schema - inspect_table - list_tables

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -2,20 +2,12 @@ import { ListToolsRequestSchema, CallToolRequestSchema } from "@modelcontextprot
 import type { Server } from "@modelcontextprotocol/sdk/server/index.js";
 import { DatabaseService } from "../services/database.js";
 import { QUERY_TOOL, handleQueryTool } from './query.js';
-import { INSPECT_DATABASE_TOOL, handleInspectDatabaseTool } from './inspectDatabase.js';
-import { INSPECT_SCHEMA_TOOL, handleInspectSchemaTool } from './inspectSchema.js';
-import { INSPECT_TABLE_TOOL, handleInspectTableTool } from './inspectTable.js';
-import { LIST_TABLES_TOOL, handleListTablesTool } from './listTables.js';
 import { RECONNECT_TOOL, handleReconnectTool } from './reconnect.js';
 import { PARTITION_LIST_TOOL, handlePartitionListTool } from './partitionList.js';
 import { TABLE_LIST_TOOL, handleTableListTool } from './tableList.js';
 import { TABLE_SHOW_TOOL, handleTableShowTool } from './tableShow.js';
 
 export * from './query.js';
-export * from './inspectDatabase.js';
-export * from './inspectSchema.js';
-export * from './inspectTable.js';
-export * from './listTables.js';
 export * from './reconnect.js';
 export * from './partitionList.js';
 export * from './tableList.js';
@@ -27,10 +19,6 @@ export function setupTools(server: Server, db: DatabaseService) {
     return {
       tools: [
         QUERY_TOOL,
-        LIST_TABLES_TOOL,
-        INSPECT_DATABASE_TOOL,
-        INSPECT_SCHEMA_TOOL,
-        INSPECT_TABLE_TOOL,
         RECONNECT_TOOL,
         PARTITION_LIST_TOOL,
         TABLE_LIST_TOOL,
@@ -46,18 +34,6 @@ export function setupTools(server: Server, db: DatabaseService) {
     switch (name) {
       case QUERY_TOOL.name:
         return handleQueryTool(db, args as { sql: string });
-
-      case INSPECT_DATABASE_TOOL.name:
-        return handleInspectDatabaseTool(db, args as { filter?: string });
-
-      case INSPECT_SCHEMA_TOOL.name:
-        return handleInspectSchemaTool(db, args as { name: string; filter?: string });
-
-      case INSPECT_TABLE_TOOL.name:
-        return handleInspectTableTool(db, args as { name: string; schema?: string });
-
-      case LIST_TABLES_TOOL.name:
-        return handleListTablesTool(db, args as { schema?: string; filter?: string });
         
       case RECONNECT_TOOL.name:
         return handleReconnectTool(db, args as { database_path?: string });
